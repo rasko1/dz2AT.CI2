795c65b5b30e61c5d2520ddd93184d782127d374
rasko1 <oophn.88@yandex.ru>
2023-07-04 18:57:03 +0300

    settings CI

--- src/test/java/postmanTest.java
+++ src/test/java/postmanTest.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class PostmanTest {
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class postmanTest {
+    @Test
+    void postman() {
+        // Given - When - Then
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"));
+    }
 }
